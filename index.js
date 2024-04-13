47e3e5df43e3381d5ecaae4302618f1cb081c04c
Muhammad  Ali Sheikh <154984806+muhammadali729@users.noreply.github.com>
2024-04-13 01:58:50 -0700

    Add files via upload

---
+++ index.js
@@ -0,0 +1,16 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+const randomnumber = Math.floor(Math.random() * 10 + 1);
+const number = await inquirer.prompt([
+    {
+        name: "userGuessNumber",
+        type: "number",
+        message: "Please guess the number between 1-10:",
+    },
+]);
+if (number.userGuessNumber === randomnumber) {
+    console.log("Congratulation!! Your Guess right number");
+}
+else {
+    console.log("Opps!! Your Guess Wrong number");
+}
